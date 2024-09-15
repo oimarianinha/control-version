fb792efb7ab5c489300163b6548780d0c77a64cd
Mariana Vaz <mariananunestaborda@gmail.com>
2024-09-15 23:21:10 +0000

    Add list products

--- backend/src/products/entities/product.entity.ts
+++ backend/src/products/entities/product.entity.ts
@@ -1,1 +1,41 @@
-export class Product {}
+import { Column, Model, Table, DataType } from 'sequelize-typescript';
+import { UUIDV4 } from 'sequelize';
+
+@Table({ 
+  tableName: 'Product', 
+  createdAt: true, 
+  updatedAt: true 
+})
+export class Product extends Model {
+  @Column({
+    type: DataType.UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4,
+    allowNull: false,
+  })
+  id: string;
+
+  @Column({
+    allowNull: false,
+    unique: true,
+  })
+  name: string;
+
+  @Column({
+    type: DataType.FLOAT, // Assuming price is a floating point number
+    allowNull: false,
+  })
+  price: number;
+
+  @Column({
+    type: DataType.STRING, // Assuming category is a string
+    allowNull: false,
+  })
+  category: string;
+
+  @Column({
+    type: DataType.FLOAT, // Assuming rating is a floating point number
+    allowNull: true,
+  })
+  rating: number;
+}
