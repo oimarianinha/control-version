ecb9baa40f4cb0d40600c27d26b45e6399124808
Mariana Vaz <mariananunestaborda@gmail.com>
2024-09-17 17:01:32 +0000

    Add flag promo

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,16 @@
-export default function Home() {
+import { blackFridayFlag } from './flags';
+
+export default async function Page() {
+  const black_friday = await blackFridayFlag();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono
-    text-sm lg:flex">
-    <h1> Welcome to the Mariana Vaz App - Deploy 2 </h1>
+    <div>
+      <h1>Welcome to the Mariana Vaz App - deploy v3</h1>
+      {black_friday ? (
+        <button>Promo</button>
+      ) : (
+        <button>Normal</button>
+      )}
     </div>
-    </main>
-  )
+  );
 }
